39749b41d50121c758ba3356762ac7be04760d74
Margi <margipancholi7@gmail.com>
2022-12-15 12:29:56 -0500

    hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -199,13 +199,13 @@ export default function Hero() {
                 href="#"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
-                Pricing
+                Villas
               </a>
               <a
                 href="#"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
-                Docs
+                Houses
               </a>
 
               <Popover className="relative">
